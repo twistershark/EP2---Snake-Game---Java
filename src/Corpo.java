02a10b75739b3b9e27330a9377004095a5165f82
TwisterShark <twistershark@TwisterSharks-MacBook-Pro.local>
2019-11-07 11:15:34 -0300

    Adiciona movimentação

--- src/Corpo.java
+++ src/Corpo.java
@@ -1,4 +1,41 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Corpo {
 
+	private int xCoor, yCoor, largura, altura;
+	
+	public Corpo(int xCoor, int yCoor, int tileSize) {
+		this.xCoor = xCoor;
+		this.yCoor = yCoor;
+		largura = tileSize;
+		altura = tileSize;
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(Color.green);
+		g.fillRect(xCoor * largura, yCoor * altura, largura, altura);
+	}
+
+	public int getxCoor() {
+		return xCoor;
+	}
+
+	public void setxCoor(int xCoor) {
+		this.xCoor = xCoor;
+	}
+
+	public int getyCoor() {
+		return yCoor;
+	}
+
+	public void setyCoor(int yCoor) {
+		this.yCoor = yCoor;
+	}
+	
+	
 }
--- src/Painel.java
+++ src/Painel.java
@@ -1,11 +1,13 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
-public class Painel extends JPanel implements Runnable{
+public class Painel extends JPanel implements Runnable, KeyListener{
 
 	private static final long serialVersionUID = 1L;
 	private static final int LARGURA = 500, ALTURA = 500;
@@ -24,8 +26,11 @@ public class Painel extends JPanel implements Runnable{
 	private int ticks = 0;
 	
 	public Painel() {
+		setFocusable(true);
+		
 		
 		setPreferredSize(new Dimension(LARGURA, ALTURA));
+		addKeyListener(this);
 		
 		snake = new ArrayList<Corpo>();
 		
@@ -54,8 +59,24 @@ public class Painel extends JPanel implements Runnable{
 			c = new Corpo(xCoor, yCoor, 10);
 			snake.add(c);
 		}
+		
 		ticks++;
 		
+		if(ticks > 300000) {
+			if(direita) xCoor++;
+			if(esquerda) xCoor--;
+			if(cima) yCoor--;
+			if(baixo) yCoor++;
+			
+			ticks = 0;
+			
+			c = new Corpo(xCoor, yCoor, 10);
+			snake.add(c);
+			
+			if(snake.size() > size) {
+				snake.remove(0);
+			}
+		}
 	}
 	
 	public void paint(Graphics g) {
@@ -75,6 +96,43 @@ public class Painel extends JPanel implements Runnable{
 			tick();
 			repaint();
 		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		if(key == KeyEvent.VK_RIGHT && !esquerda) {
+			direita = true;
+			cima = false;
+			baixo = false;
+		}
+		if(key == KeyEvent.VK_LEFT && !direita) {
+			esquerda = true;
+			cima = false;
+			baixo = false;
+		}
+		if(key == KeyEvent.VK_UP && !baixo) {
+			cima = true;
+			esquerda = false;
+			direita = false;
+		}
+		if(key == KeyEvent.VK_DOWN && !cima) {
+			baixo = true;
+			esquerda = false;
+			direita = false;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	

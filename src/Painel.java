1abb4dda80907d055a549bca5fae0847a7012d66
Paulo Victor da Silva <paulovmel@gmail.com>
2019-11-11 23:46:02 -0300

    Retira includes desnecessários

--- src/Painel.java
+++ src/Painel.java
@@ -7,8 +7,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
